30b2ad76a007340961517ddcd202554140b334aa
absoufiane <soufianeabderrahman2@gmail.com>
2021-11-01 16:09:56 +0000

    Update assignment1.js

--- Assignment 3/assignment1.js
+++ Assignment 3/assignment1.js
@@ -1,13 +1,12 @@
 // Function the game 
 function game() {
-    var num = parseInt(Math.random() * 10 + 1);
-    var inputNumber = prompt("input a guess number between 1 to 10");
+    var num = parseInt(Math.random() * 10 + 1);  //Num is a variable so that the number is between 10 and 1
+    var inputNumber = prompt("input a guess number between 1 to 10");  //Variable 
     if(isNaN(inputNumber)) {
     alert("Please, write a Number");
-    
     } else {
         if(num == inputNumber) {
-            alert("Good Work");
+            alert("Good Work Number is = " + num);
         } else {
             alert("Not matched Number is = " + num);
         }
